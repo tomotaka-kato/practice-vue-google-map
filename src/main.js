14664c2d9ee53aa75410b8cee3eb8c450538ad49
加藤友崇 <tomo@katoutomotakashinoMacBook-Pro.local>
2017-12-04 19:59:53 +0900

    マーカー表示

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
-
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
